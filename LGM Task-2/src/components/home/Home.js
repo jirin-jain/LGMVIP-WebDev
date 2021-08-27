492912c1cd05009a1e5996f70befeca859ab5c75
jirin-jain <86232026+jirin-jain@users.noreply.github.com>
2021-08-28 00:49:26 +0530

    Add files via upload

---
+++ LGM Task-2/src/components/home/Home.js
@@ -0,0 +1,13 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="content">
+        Click <span> Get Users</span> button <br /> to fetch users
+      </div>
+    </div>
+  );
+};
+
+export default Home;
